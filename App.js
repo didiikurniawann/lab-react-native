fefc3dbefe8567e9d145cd2f6a8028d7a93fa18d
Didi Kurniawan <didikurniawan888@gmail.com>
2018-09-13 10:19:56 +0700

    aktifitas 13/07/2018

--- App.js
+++ App.js
@@ -6,68 +6,31 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
+
   StyleSheet,
-  Text,
   View,
-  Image,
-  ImageBackGround
+  StatusBar
 } from 'react-native';
-import Judul from './Components/Judul';
+import Routes from './source/Routes';
 
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
     <View style={styles.container} >
-    <Judul/>
-    <Judul title="BIODATA"/>
-    <Judul title="PROFILE"/>
-    <Text style={styles.welcome}>
-     Nama = Didi Kurniawan
-    </Text>
-    
-    <Text style={styles.welcome}>
-     Kelas = XI RPL 3 
-    </Text>
-
-    <Text style={styles.welcome}>
-     No Absen = 14 
-    </Text>
-    <Image source={require('./fotodidi.jpg')} style={{width: 300, height: 300}}/>
-
-
-    
-
-  </View>
+    <StatusBar
+           backgroundColor="#1c313a"
+           barStyle="light-content"
+         />
+        <Routes/>
+    </View>
     );
   }
 
 }
-
 const styles = StyleSheet.create({
-  container: {
+  container : {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ff7675',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'right',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#192a56',
-    marginBottom: 5,
+
+
   },
 });
